26efd6b9743196df3605b7f62ef88f8916fe0bbb
Rasheednour <rasheednour@gmail.com>
2023-07-22 00:02:59 +0200

    Exercise 1.11: Finished

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -14,9 +14,10 @@ const Button = ({ text, feedback, setFeedback }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      {text} {value}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td> {value}</td>
+    </tr>
   );
 };
 
@@ -35,12 +36,16 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return (
     <div>
-      <StatisticLine text={"good"} value={good} />
-      <StatisticLine text={"neutral"} value={neutral} />
-      <StatisticLine text={"bad"} value={bad} />
-      <StatisticLine text={"all"} value={all} />
-      <StatisticLine text={"average"} value={average} />
-      <StatisticLine text={"positive"} value={positive} />
+      <table>
+        <tbody>
+          <StatisticLine text={"good"} value={good} />
+          <StatisticLine text={"neutral"} value={neutral} />
+          <StatisticLine text={"bad"} value={bad} />
+          <StatisticLine text={"all"} value={all} />
+          <StatisticLine text={"average"} value={average} />
+          <StatisticLine text={"positive"} value={positive} />
+        </tbody>
+      </table>
     </div>
   );
 };
