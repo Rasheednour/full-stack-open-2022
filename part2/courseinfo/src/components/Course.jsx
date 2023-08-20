554215760d92308d7ba16f85d680abd3dfb85217
Rasheednour <rasheednour@gmail.com>
2023-08-20 13:57:09 +0200

    Exercise 2.2: Finished

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const Header = ({ course }) => <h1>{course}</h1>;
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+const Total = ({ parts }) => {
+  const sum = parts.reduce((sum, part) => part.exercises + sum, 0);
+  return (
+    <p>
+      <b>total of {sum} exercises</b>
+    </p>
+  );
+};
 
 const Part = ({ part }) => {
   return (
@@ -18,6 +25,7 @@ const Content = ({ parts }) => {
       {parts.map((part) => (
         <Part key={part.id} part={part} />
       ))}
+      <Total parts={parts} />
     </>
   );
 };
