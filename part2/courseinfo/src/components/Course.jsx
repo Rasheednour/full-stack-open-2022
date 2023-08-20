87f807a60c64adcf1c0774a3055d3974aa794b6b
Rasheednour <rasheednour@gmail.com>
2023-08-20 13:39:51 +0200

    Exercise 2.1: Finished

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+export default Course;
