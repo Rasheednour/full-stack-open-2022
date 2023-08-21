11172cb761ee00afeb6a60cbd831ebe9d4b9875a
Rasheednour <rasheednour@gmail.com>
2023-08-21 22:03:05 +0200

    Exercise 2.12: Finished

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -46,9 +46,15 @@ const App = () => {
       }
     }
     if (!nameExists) {
-      setPersons(persons.concat({ name: newName, number: newNumber }));
-      setNewName("");
-      setNewNumber("");
+      const personObject = { name: newName, number: newNumber }
+      axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response=>{
+        setPersons(persons.concat(response.data))
+        setNewName("");
+        setNewNumber("");
+      })
+      
     }
   };
   return (
