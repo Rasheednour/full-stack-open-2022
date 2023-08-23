5b061b44f30ae3030a2a5e808406e917a3a23c3f
Rasheednour <rasheednour@gmail.com>
2023-08-24 01:54:37 +0200

    Exercise 2.20: Finished

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -2,11 +2,21 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 const Country = ({ country }) => {
   const [data, setData] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+  const api_key = import.meta.env.VITE_SOME_KEY;
   useEffect(() => {
     axios
       .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
       .then((response) => {
-        setData(response.data);
+        const countryData = response.data;
+        setData(countryData);
+        axios
+          .get(
+            `https://api.openweathermap.org/data/3.0/onecall?lat=${countryData.latlng[0]}&lon=${countryData.latlng[1]}&exclude=minutely,hourly,daily,alerts&units=metric&appid=${api_key}`
+          )
+          .then((response) => {
+            setWeatherData(response.data);
+          });
       });
   }, []);
   return (
@@ -23,6 +33,16 @@ const Country = ({ country }) => {
             ))}
           </ul>
           <img src={data.flags.png} alt={data.flags.alt}></img>
+          <h3>Weather in {data.name.common}</h3>
+          {weatherData && (
+            <div>
+              <p>temperature {weatherData.current.temp} Celcius</p>
+              <img
+                src={`https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`}
+              ></img>
+              <p>wind {weatherData.current.wind_speed} m/s</p>
+            </div>
+          )}
         </div>
       )}
     </>
