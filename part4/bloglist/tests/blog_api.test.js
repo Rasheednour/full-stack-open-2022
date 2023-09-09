7929caf3cd0a9e6f0f50a42cb0996f999e190ec5
Rasheednour <rasheednour@gmail.com>
2023-09-09 22:57:19 +0200

    Exercise 4.10: Finished

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -41,6 +41,30 @@ test("the unique identifier property of blog posts is named id", async () => {
   }
 });
 
+test("Making a POST request creates a new blog post", async () => {
+  const newBlog = {
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
+    likes: 10,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/blogs");
+
+  const urls = response.body.map((blog) => blog.url);
+
+  expect(response.body).toHaveLength(initialBlogs.length + 1);
+  expect(urls).toContain(
+    "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html"
+  );
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
