ff08c8b674bdd01fb265e0980ef55ee3942853bc
Rasheednour <rasheednour@gmail.com>
2023-09-14 22:02:29 +0200

    Exercise 4.17: Finished

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,15 +1,25 @@
 const blogsRouter = require("express").Router();
 const { response } = require("../app");
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+  const users = await User.find({});
+  if (!users) {
+    return response.status(404).json({ error: "no users found" });
+  }
+  const user = users[0];
+  const blogObject = request.body;
+  blogObject.user = user.id;
+  const blog = new Blog(blogObject);
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
   response.status(201).json(savedBlog);
 });
 
