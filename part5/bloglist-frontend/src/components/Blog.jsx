0602e34686ec1e6da26644208f28e6fa7c3b1406
Rasheednour <rasheednour@gmail.com>
2023-09-17 15:45:17 +0200

    Exercise 5.7: Finished

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [isHidden, setIsHidden] = useState(true);
+
+  const toggleDisplay = () => {
+    setIsHidden(!isHidden);
+  };
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  return (
+    <div style={blogStyle}>
+      <span>
+        {blog.title} {blog.author}
+      </span>
+      <button onClick={toggleDisplay}>{isHidden ? "view" : "hide"}</button>
+      {!isHidden && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes}
+            <button>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
