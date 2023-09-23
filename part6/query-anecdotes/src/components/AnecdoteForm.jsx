4b4dc89ddc26dc4e48cab1af0206d0d5d4d3c91e
Rasheednour <rasheednour@gmail.com>
2023-09-23 21:48:21 +0200

    Exercise 6.21: Finsihed

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,20 @@
+import { createAnecdote } from "../requests"
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    newAnecdoteMutation.mutate({ content: content, votes: 0 })
+    
 }
 
   return (
